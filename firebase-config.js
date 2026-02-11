be97c058f195fa47f899b3f464f719c3eacff03a
Musicalbase <musicalbasevr@gmail.com>
2026-02-11 04:02:49 +0100

    App movil inicial

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyC5KoU8YyrSwRIjuhMczS8mnEBgMfDlrzc",
+    authDomain: "plataforma-examenes-f2df9.firebaseapp.com",
+    projectId: "plataforma-examenes-f2df9",
+    storageBucket: "plataforma-examenes-f2df9.firebasestorage.app",
+    messagingSenderId: "504614396126",
+    appId: "1:504614396126:web:2d526051d5c7503e21224f"
+};
