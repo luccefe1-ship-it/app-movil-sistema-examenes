b047ea379d09c478108828679c99c2854a93e9c0
luccefe1-ship-it <luccefe1@gmail.com>
2026-02-14 01:55:24 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'examenes-app-v1';
+const CACHE_NAME = 'examenes-app-v2';
 const ASSETS = [
     '/',
     '/index.html',
@@ -8,13 +8,16 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', e => {
+    self.skipWaiting();
     e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
 });
 
 self.addEventListener('activate', e => {
-    e.waitUntil(caches.keys().then(keys =>
-        Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    ));
+    e.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+        ).then(() => self.clients.claim())
+    );
 });
 
 self.addEventListener('fetch', e => {
