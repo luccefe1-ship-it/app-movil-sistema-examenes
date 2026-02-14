ff04c7e2fcf88a605596ec85243cb59ad0152ed4
luccefe1-ship-it <luccefe1@gmail.com>
2026-02-14 02:53:00 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,30 +1,4 @@
-const CACHE_NAME = 'examenes-app-v7';
-const ASSETS = [
-    '/',
-    '/index.html',
-    '/firebase-config.js',
-    '/manifest.json'
-];
-
-self.addEventListener('install', e => {
-    self.skipWaiting();
-    e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
-});
-
-self.addEventListener('activate', e => {
-    e.waitUntil(
-        caches.keys().then(keys =>
-            Promise.all(keys.map(k => caches.delete(k)))
-        ).then(() => self.clients.claim())
-    );
-});
-
+// Service Worker desactivado - sin caché
 self.addEventListener('fetch', e => {
-    if (e.request.url.includes('app.html')) {
-        e.respondWith(fetch(e.request));
-        return;
-    }
-    e.respondWith(
-        fetch(e.request).catch(() => caches.match(e.request))
-    );
+    e.respondWith(fetch(e.request));
 });
