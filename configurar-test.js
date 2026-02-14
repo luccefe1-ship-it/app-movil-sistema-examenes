b1084b7d14d039395022a1e7fccd67ce7023823b
luccefe1-ship-it <luccefe1@gmail.com>
2026-02-14 07:56:29 +0100

    Update configurar-test.js

--- configurar-test.js
+++ configurar-test.js
@@ -1,4 +1,4 @@
-import { auth, db } from './js/firebase-config.js';
+import { auth, db } from './firebase-config.js';
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { collection, query, where, getDocs } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
